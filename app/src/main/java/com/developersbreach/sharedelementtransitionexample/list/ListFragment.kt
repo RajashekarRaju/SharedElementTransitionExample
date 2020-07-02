a08cba83e7c6ad9090d38c365f2af88f9635fa4b
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-02 17:14:51 +0530

    Populated data in fragment with RecyclerView, added navigation direction with args

--- app/src/main/java/com/developersbreach/sharedelementtransitionexample/list/ListFragment.kt
+++ app/src/main/java/com/developersbreach/sharedelementtransitionexample/list/ListFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.developersbreach.sharedelementtransitionexample.R
 
 
 class ListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +21,20 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.sports_recycler_view)
+        val list = Sports.sportsList(requireContext())
+        val adapter = SportsAdapter(list, sportsItemListener)
+        recyclerView.adapter = adapter
+    }
+
+    private val sportsItemListener = SportsAdapter.OnClickListener { sports ->
+        val direction: NavDirections =
+            ListFragmentDirections.listToDetailFragment(sports)
+        findNavController().navigate(direction)
     }
 }
