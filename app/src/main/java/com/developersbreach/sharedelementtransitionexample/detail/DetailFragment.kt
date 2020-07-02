480a527ee3dc3b6cb2b56955e2bb9dd60de09617
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-02 17:15:34 +0530

    Populated views data with fragment arguments received

--- app/src/main/java/com/developersbreach/sharedelementtransitionexample/detail/DetailFragment.kt
+++ app/src/main/java/com/developersbreach/sharedelementtransitionexample/detail/DetailFragment.kt
@@ -5,10 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import com.developersbreach.sharedelementtransitionexample.R
-
+import com.developersbreach.sharedelementtransitionexample.list.Sports
 
 class DetailFragment : Fragment() {
+
+    private lateinit var sportsArgs: Sports
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val args = requireArguments()
+        sportsArgs = DetailFragmentArgs.fromBundle(args).modelArgs
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +26,17 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val banner: ImageView = view.findViewById(R.id.detail_image_view)
+        val title: TextView = view.findViewById(R.id.title_detail_text_view)
+        val about: TextView = view.findViewById(R.id.about_detail_text_view)
+
+        banner.setImageResource(sportsArgs.banner)
+        title.text = sportsArgs.title
+        about.text = sportsArgs.about
     }
 }
