1dda2afd963f4135430b32028eb083bc23371784
Rajasekhar <rajashekarraju123@gmail.com>
2020-07-02 16:34:51 +0530

    Created NavigationGraph and attached with Activity NavigationController

--- app/src/main/java/com/developersbreach/sharedelementtransitionexample/MainActivity.kt
+++ app/src/main/java/com/developersbreach/sharedelementtransitionexample/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.developersbreach.sharedelementtransitionexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
